8e231c26085e42afc79ac72450fc51fa3f442955
Athos Jasper <96916791+Naemon4@users.noreply.github.com>
2025-05-20 12:58:59 -0300

    Add files via upload

---
+++ public/js/headerFooter.js
@@ -0,0 +1,44 @@
+function criarHeader(){
+
+    document.getElementById("header").innerHTML = `
+    
+        <nav>
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="aboutUs.html">Sobre nós</a></li>
+                <li><a href="produtos.html">Loja</a></li>
+                <li><a href="login.html">Login</a></li>
+            </ul>
+        </nav>
+    
+    `
+
+}
+
+function criarFooter(){
+
+    document.getElementById("footer").innerHTML = `
+    
+        <ul>
+            <li><a href="https://www.instagram.com/"><img src="../img/instaIcon.png" alt="link do instagram da empresa"></a></li>
+            <li>12 12345-6789</li>
+            <li>OPLoja@email.com</li>
+        </ul>
+    
+    `
+
+}
+
+function linkarStyles(){
+
+    document.head.innerHTML += `<link rel="stylesheet" href="../css/headerFooter.css">`
+
+}
+
+function criarHeaderFooter(){
+
+    criarHeader()
+    criarFooter()
+    linkarStyles()
+
+}
